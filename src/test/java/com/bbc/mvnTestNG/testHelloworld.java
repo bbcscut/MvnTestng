a93d67a86d28a366919f99168639468df67eda40
bbcsc <bbcsc@LAPTOP-QVJH7I2R>
2018-05-15 15:08:50 +0800

    a maven testng demonstration

---
+++ src/test/java/com/bbc/mvnTestNG/testHelloworld.java
@@ -0,0 +1,10 @@
+package com.bbc.mvnTestNG;
+
+import org.testng.annotations.Test;
+
+public class testHelloworld {
+	@Test
+	public void name() {
+		System.out.println("helloworld1");
+	}
+}
---
+++ src/test/java/com/bbc/mvnTestNG/testHelloworld2.java
@@ -0,0 +1,11 @@
+package com.bbc.mvnTestNG;
+
+import org.testng.annotations.Test;
+
+
+public class testHelloworld2 {
+	@Test
+	public void name() {
+	System.out.println("helloworld2");	
+	}
+}
